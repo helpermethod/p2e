9d10edca1d0af40f985fd913873acca534e72550
helpermethod <oliver@sdkman.io>
2022-06-24 17:20:12 +0200

    Add initial GitHub workflow

--- p2e.java
+++ p2e.java
@@ -7,7 +7,7 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "p2e", mixinStandardHelpOptions = true, version = "p2e 0.1",
+@Command(name = "p2e", mixinStandardHelpOptions = true, version = "0.1.0",
         description = "Convert Spring application property names to environment variable names.")
 class p2e implements Runnable {
     @Parameters(index = "0", description = "The property name")

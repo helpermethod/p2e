5e05df292ef3eac49374b1a23554ce611d69cdf8
helpermethod <oliver@sdkman.io>
2022-09-13 12:07:28 +0200

    Implement custom IVersionProvider

--- p2e.java
+++ p2e.java
@@ -1,17 +1,21 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.6.3
 //DEPS info.picocli:picocli-codegen:4.6.3
+//MANIFEST Implementation-Version=${version}
 //JAVA 17
+
+package com.github.helpermethod.p2e;
 
 import java.util.Locale;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.IVersionProvider;
 import picocli.CommandLine.Parameters;
 
 @Command(
     name = "p2e",
     mixinStandardHelpOptions = true,
-    version = "0.1.0", 
+    versionProvider = p2e.ManifestVersionProvider.class, 
     description = "Convert Spring configuration property names to environment variable names."
 )
 class p2e implements Runnable {
@@ -33,5 +37,11 @@ class p2e implements Runnable {
                 .toUpperCase(Locale.ENGLISH);
 
         System.out.println(environmentVariableName);
+    }
+
+    static class ManifestVersionProvider implements IVersionProvider {
+        public String[] getVersion() {
+            return new String[] { getClass().getPackage().getImplementationVersion() };
+        }
     }
 }
